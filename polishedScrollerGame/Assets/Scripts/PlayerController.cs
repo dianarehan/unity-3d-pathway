2c86e44cdd8e038f2469dc64549d31fda8b8de0a
diana <daiana.rehan.dr@gmail.com>
2024-06-22 19:03:17 +0300

    first lesson done

---
+++ polishedScrollerGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    
+    [SerializeField] float jumpForce = 10f;
+    [SerializeField] float gravityModifier;
+    private bool isGrounded = true;
+    void Start()
+    {
+        rb= GetComponent<Rigidbody>();
+        Physics.gravity*=gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float signal = Input.GetAxis("Jump");
+        //Jump(signal);
+        if(Input.GetKeyDown(KeyCode.Space)&& isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+        
+        
+    }
+    private void Jump(float signal)
+    {
+        if (signal>0)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+        
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
