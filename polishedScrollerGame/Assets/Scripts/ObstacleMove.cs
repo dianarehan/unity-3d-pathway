9ba21b6002a932ea580f6843affc5d27bfe7c409
diana <daiana.rehan.dr@gmail.com>
2024-06-22 19:32:08 +0300

    obstacle placed

---
+++ polishedScrollerGame/Assets/Scripts/ObstacleMove.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMove : MonoBehaviour
+{
+    [SerializeField] GameObject obstacle;
+    void Start()
+    {
+        
+    }
+
+    
+}
