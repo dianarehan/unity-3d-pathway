97891c999ec2a528c1d977d9a1e5ed5523701659
diana <daiana.rehan.dr@gmail.com>
2024-06-24 02:57:02 +0300

    challenge is done + bonuses

--- polishedScrollerGame/Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ polishedScrollerGame/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -16,14 +16,14 @@ public class PlayerControllerX : MonoBehaviour
     private AudioSource playerAudio;
     public AudioClip moneySound;
     public AudioClip explodeSound;
-
+    public AudioClip boingSound;
 
     // Start is called before the first frame update
     void Start()
     {
         Physics.gravity *= gravityModifier;
         playerAudio = GetComponent<AudioSource>();
-
+        playerRb= GetComponent<Rigidbody>();
         // Apply a small upward force at the start of the game
         playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
 
@@ -33,9 +33,9 @@ public class PlayerControllerX : MonoBehaviour
     void Update()
     {
         // While space is pressed and player is low enough, float up
-        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        if (Input.GetKeyDown(KeyCode.Space) && !gameOver && gameObject.transform.position.y<14)
         {
-            playerRb.AddForce(Vector3.up * floatForce);
+            playerRb.AddForce(Vector3.up * floatForce, ForceMode.Impulse);
         }
     }
 
@@ -58,6 +58,11 @@ public class PlayerControllerX : MonoBehaviour
             playerAudio.PlayOneShot(moneySound, 1.0f);
             Destroy(other.gameObject);
 
+        }
+        else if (other.gameObject.CompareTag("Ground"))
+        {
+            playerRb.AddForce(Vector3.up * 200, ForceMode.Impulse);
+            playerAudio.PlayOneShot(boingSound, 1.0f);  
         }
 
     }
